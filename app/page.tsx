b9765f40280593bd4f99129693cd40d26ca2f134
Mustafa alhasan <mustafa.alhasan.2020@gmail.com>
2024-06-15 14:59:09 +0300

    PubV1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,190 @@
-import Image from "next/image";
+// app/page.tsx
 
-export default function Home() {
+"use client"
+
+import * as React from "react"
+import { useRouter } from "next/navigation"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "../components/ui/card"
+import { Button } from "../components/ui/button"
+import { Input } from "../components/ui/input"
+import { ToastProvider, Toast, ToastTitle, ToastDescription, ToastViewport, useToast } from "../components/ui/toast"
+import Image from "next/image"
+import { genPublic, genShared, encryption, decryption } from "../lib/crypto-utils"
+import { useKeyContext } from "../context/KeyContext"
+import {jwtDecode} from 'jwt-decode'; // Import jwt-decode
+
+const LoginPage = () => {
+  const [email, setEmail] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  const [step, setStep] = React.useState(1)
+  const [loading, setLoading] = React.useState(false)
+  const router = useRouter()
+  const { open, message, setOpen, showToast } = useToast()
+  const { sharedKey, jwt, setServerPublicKey, setSharedKey, setJwt, setRole } = useKeyContext() // Add setRole
+
+  const handleEmailSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      const clientPublicKey = await genPublic();
+
+      const response = await fetch("https://fuse-backend-x7mr.onrender.com/key/dashboard/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, clientPublicKey }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Server responded with status ${response.status}`);
+      }
+
+      const data = await response.json();
+      const { serverPublicKey } = data;
+
+      setServerPublicKey(serverPublicKey);
+
+      const derivedSharedKey = await genShared(serverPublicKey);
+      setSharedKey(derivedSharedKey);
+
+      setStep(2);
+    } catch (error) {
+      console.error('Error during key exchange:', error);
+      showToast("Error", "Failed to exchange keys. Please try again.", "destructive");
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handlePasswordSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      if (sharedKey) {
+        const encryptedPayload = await encryption({ data: { email, password } }, sharedKey);
+
+        const response = await fetch("https://fuse-backend-x7mr.onrender.com/auth/dashboard/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, payload: encryptedPayload }),
+        });
+
+        if (response.ok) {
+          const decryptedData = await decryption(await response.json(), sharedKey);
+          const parsedData = JSON.parse(decryptedData);
+          const { jwt } = parsedData;
+          setJwt(jwt);
+
+          // Decode the JWT token to extract the role
+          const decodedToken: any = jwtDecode(jwt);
+          const { role } = decodedToken;
+          console.log("Decoded role:", role); // Debug: Print the extracted role
+          setRole(role); // Save the role in context
+
+          router.push("/Dashboard/home");
+        } else {
+          const errorData = await response.json();
+          console.error('Error response:', errorData);
+          showToast("Invalid credentials", "Please check your email and password and try again.", "destructive");
+        }
+      } else {
+        showToast("Error", "Encryption key not established. Please try again.", "destructive");
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      showToast("Error", "Failed to login. Please try again.", "destructive");
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <ToastProvider>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <Card className="w-full max-w-md p-6">
+          <CardHeader>
+            <div className="flex items-center justify-between">
+              <div>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Access your account
+                </CardDescription>
+              </div>
+              <Image
+                priority={true}
+                src="/FuseLogo.png"
+                alt="Fuse Logo"
+                width={50}
+                height={50}
+                className="ml-2"
+              />
+            </div>
+          </CardHeader>
+          <CardContent>
+            {step === 1 ? (
+              <form onSubmit={handleEmailSubmit}>
+                <div className="mb-4">
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full"
+                  />
+                </div>
+                <Button type="submit" className="w-full" disabled={loading}>
+                  {loading ? (
+                    <>
+                      <span className="mr-2">Loading...</span>
+                      <div className="spinner-border animate-spin inline-block w-4 h-4 border-4 border-t-transparent border-white rounded-full"></div>
+                    </>
+                  ) : (
+                    "Next"
+                  )}
+                </Button>
+              </form>
+            ) : (
+              <form onSubmit={handlePasswordSubmit}>
+                <div className="mb-4">
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full"
+                  />
+                </div>
+                <Button type="submit" className="w-full" disabled={loading}>
+                  {loading ? (
+                    <>
+                      <span className="mr-2">Loading...</span>
+                      <div className="spinner-border animate-spin inline-block w-4 h-4 border-4 border-t-transparent border-white rounded-full"></div>
+                    </>
+                  ) : (
+                    "Login"
+                  )}
+                </Button>
+              </form>
+            )}
+          </CardContent>
+        </Card>
+        <ToastViewport />
+        <Toast open={open} onOpenChange={setOpen} variant={message.variant}>
+          <ToastTitle>{message.title}</ToastTitle>
+          <ToastDescription>{message.description}</ToastDescription>
+        </Toast>
+      </div>
+      {jwt && (
+        <div className="mt-4 p-4 bg-white shadow-md rounded-md">
+          <h2 className="text-lg font-bold">JWT Token:</h2>
+          <pre className="break-words">{jwt}</pre>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      )}
+    </ToastProvider>
+  )
 }
+
+export default LoginPage
